b35b9994318121b68f3d1464df665846fa7b0072
sandesh-8622 <sandesh.bhandari3388@gmail.com>
2025-12-01 09:14:00 +0000

    initial commit

---
+++ medium2pdf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+medium2pdf - archive a medium author's posts as pdfs
+"""
+
+import sys
+
+def main():
+    print("medium2pdf")
+
+if __name__ == "__main__":
+    main()
